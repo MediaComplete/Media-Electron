b59b315bdb21ebf09a505f8b41812e059f01f480
tyler.evert <tyler.evert@centare.com>
2016-02-26 15:24:07 -0600

    Adding icons for the window bar buttons and resizeable regions.

--- app/app.js
+++ app/app.js
@@ -9,17 +9,13 @@ import { greet } from './hello_world/hello_world';
 // are required the same way as always.
 var os = require('os');
 var jetpack = require('fs-jetpack');
+var $ = require('jquery');
+require('jquery-ui');
 
 // Holy crap! This is browser window with HTML and stuff, but I can read
 // here files like it is node.js! Welcome to Electron world :)
 var manifest = require('./package.json');
 
-// window.env contains data from config/env_XXX.json file.
-var envName = window.env.name;
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('greet').innerHTML = greet();
-    document.getElementById('platform-info').innerHTML = os.platform();
-    document.getElementById('env-name').innerHTML = envName;
-    document.getElementById('app-version').innerHTML = manifest.version;
+$(function() {
+  $(".resizable").resizable();
 });
